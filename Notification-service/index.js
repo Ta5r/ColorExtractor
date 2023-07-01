33f31c076b1b6bc9722da40ee06d67036d2df27a
Tanay Srivastava <srv.tanay@gmail.com>
2023-07-01 16:27:01 +0530

    rabbitMq integ

--- Notification-service/index.js
+++ Notification-service/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const amqp = require('amqplib');
+const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5672';
 
 const app = express();
 
+
 app.get("/", (req, res) => {
+  connect();
   res.send("NOTIFCATIONS API")
 })
+
+async function connect() {
+  try {
+    const connection = await amqp.connect(amqpUrl);
+    const channel = await connection.createChannel();
+    await channel.assertQueue("order.shipped");
+    channel.consume("order.shipped", (message) => {
+      console.log({ message: message.content.toString() });
+      channel.ack(message);
+    });
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+
 
 app.listen(8001, () => {
   console.log("Listening on PORT 8001");
--- Orders-service/index.js
+++ Orders-service/index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
 
+const amqp = require('amqplib');
+const amqpUrl = process.env.AMQP_URL || 'amqp://localhost:5672';
+
 app.get('/', async (req, res) => {
-    res.send("ORDERS API")
-})
+    const connection = await amqp.connect(amqpUrl);
+    const channel = await connection.createChannel();
+    channel.assertQueue("order.shipped", {durable: true});
+    channel.sendToQueue("order.shipped", Buffer.from(JSON.stringify({
+        customerId: 5,
+        orderId: 7,
+        number: "199 222 3333"
+    })));
+    res.send("ORDERS API");
+  })
 
 app.listen(8000, () => {
     console.log("ORDERS API listening on port 8000")
